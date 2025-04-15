8bed70b9cecf3a36c606f72397a5eeba5f5888e9
leonardouwz <lpacharig@ulasalle.edu.pe>
2025-04-15 12:07:07 -0500

    Inicialización del ejercicio 1

---
+++ Practica-4/Ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -0,0 +1,17 @@
+/**
+Descripción del problema:
+Autor:                          Leonardo Pachari
+Fecha de creación:              15/04/25
+Fecha última de modificación:   15/04/25
+ */
+
+package com.example.ejercicio1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
