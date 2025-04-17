e3c10008094bd06b2f11ade77edf84bcc10c7537
leonardouwz <lpacharig@ulasalle.edu.pe>
2025-04-17 13:42:11 -0500

    Creando FormularioActivity

--- Practica-4/Ejercicio1/app/src/main/java/com/example/ejercicio1/FormularioActivity.kt
+++ Practica-4/Ejercicio1/app/src/main/java/com/example/ejercicio1/FormularioActivity.kt
@@ -1,7 +1,7 @@
 /**
-Descripción:                    Actividad de inicio del programa
+Descripción:                    Actividad para recibir los datos del perfil de usuario
 Autor:                          Leonardo Pachari
-Fecha de creación:              15/04/25
+Fecha de creación:              17/04/25
 Fecha última de modificación:   17/04/25
  */
 
@@ -10,7 +10,7 @@ package com.example.ejercicio1
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class FormularioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
