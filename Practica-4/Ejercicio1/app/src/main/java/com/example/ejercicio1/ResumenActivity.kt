c5e40e5ed2ddf14e877b1c0d5fb92b90cafc1452
leonardouwz <lpacharig@ulasalle.edu.pe>
2025-04-17 13:42:29 -0500

    Creando ResumenActivity

---
+++ Practica-4/Ejercicio1/app/src/main/java/com/example/ejercicio1/ResumenActivity.kt
@@ -0,0 +1,10 @@
+package com.example.ejercicio1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResumenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
