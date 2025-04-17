b9d59f2ec63b69f64fdf070017d566fbfd59182e
leonardouwz <lpacharig@ulasalle.edu.pe>
2025-04-17 14:42:40 -0500

    Añadiendo un boton en activity_main para pasar a la siguiente actividad

--- Practica-4/Ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ Practica-4/Ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -8,10 +8,23 @@ Fecha última de modificación:   17/04/25
 package com.example.ejercicio1
 
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnComenzar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnComenzar = findViewById(R.id.btn_Comenzar)
+
+        btnComenzar.setOnClickListener {
+            val intent = Intent(this, FormularioActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
