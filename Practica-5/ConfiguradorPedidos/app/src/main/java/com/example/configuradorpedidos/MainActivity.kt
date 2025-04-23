0f4ed64d014f9cbde6f18cde839cb03772a85d7b
leonardouwz <lpacharig@ulasalle.edu.pe>
2025-04-23 12:28:02 -0500

    Creando proyecto de la practica 6

---
+++ Practica-5/ConfiguradorPedidos/app/src/main/java/com/example/configuradorpedidos/MainActivity.kt
@@ -0,0 +1,18 @@
+/**
+ * Actividad principal de la aplicación de configuración de pedidos
+ * Autor: Leonardo Pachari
+ * Fecha creación: 23/04/2025
+ * Última modificación: 23/04/2025
+ */
+
+package com.example.configuradorpedidos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
