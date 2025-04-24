24c8d975fc75f001b63d204b83bbf1ac74cc25b0
leonardouwz <lpacharig@ulasalle.edu.pe>
2025-04-24 00:14:49 -0500

    Revert "Configurando la navegación del fragmento en MainActivity"
    This reverts commit 0d26be7ae05d37be8440c4b7000fb51cb126bc14.

--- Practica-5/ConfiguradorPedidos/app/src/main/java/com/example/configuradorpedidos/MainActivity.kt
+++ Practica-5/ConfiguradorPedidos/app/src/main/java/com/example/configuradorpedidos/MainActivity.kt
@@ -9,17 +9,10 @@ package com.example.configuradorpedidos
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Si necesitamos configurar la barra de herramientas
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        // Configuración adicional si se requiere
     }
 }
