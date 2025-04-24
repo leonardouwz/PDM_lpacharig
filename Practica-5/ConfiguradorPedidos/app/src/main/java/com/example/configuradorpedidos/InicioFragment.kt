b2691b0835c04b7e3a6e2214b4afa74ab522829d
leonardouwz <lpacharig@ulasalle.edu.pe>
2025-04-24 00:56:09 -0500

    Añadiendo fragmento de inicio y navegación a selección de comida

--- Practica-5/ConfiguradorPedidos/app/src/main/java/com/example/configuradorpedidos/InicioFragment.kt
+++ Practica-5/ConfiguradorPedidos/app/src/main/java/com/example/configuradorpedidos/InicioFragment.kt
@@ -7,8 +7,36 @@
 
 package com.example.configuradorpedidos
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
-class InicioFragment : Fragment(R.layout.fragment_inicio) {
+class InicioFragment : Fragment() {
 
+    private var _binding: FragmentInicioBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentInicioBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnNuevoPedido.setOnClickListener {
+            findNavController().navigate(R.id.action_inicioFragment_to_seleccionComidaFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
