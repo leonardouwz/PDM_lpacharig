309df2b780b9bfbeda45177fade20bedefb62603
leonardouwz <lpacharig@ulasalle.edu.pe>
2025-05-11 01:19:54 -0500

    Creando fragmentos necesarios para el juego

---
+++ Examen_Parcial/app/src/main/java/com/example/juegodecolores/ResultFragment.kt
@@ -0,0 +1,14 @@
+/*
+ * Juego de Colores - Fragmento de resultados
+ * Autor: Leonardo Pachari
+ * Fecha creación: 11/05/2025
+ * Última modificación: 11/05/2025
+ */
+
+package com.example.juegodecolores
+
+import androidx.fragment.app.Fragment
+
+class ResultFragment : Fragment(R.layout.fragment_result) {
+
+}
---
+++ Examen_Parcial/app/src/main/java/com/example/juegodecolores/WelcomeFragment.kt
@@ -0,0 +1,14 @@
+/*
+ * Juego de Colores - Fragmento de bienvenida
+ * Autor: Leonardo Pachari
+ * Fecha creación: 11/05/2025
+ * Última modificación: 11/05/2025
+ */
+
+package com.example.juegodecolores
+
+import androidx.fragment.app.Fragment
+
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+
+}
