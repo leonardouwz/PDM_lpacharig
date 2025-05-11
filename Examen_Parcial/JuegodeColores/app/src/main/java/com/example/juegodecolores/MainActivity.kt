d0d7881af7abfd71c99e79f1ccf72e34291a356b
leonardouwz <lpacharig@ulasalle.edu.pe>
2025-05-11 13:56:38 -0500

    Implementando la actividad principal que aloja a los fragmentos

--- Examen_Parcial/JuegodeColores/app/src/main/java/com/example/juegodecolores/MainActivity.kt
+++ Examen_Parcial/JuegodeColores/app/src/main/java/com/example/juegodecolores/MainActivity.kt
@@ -1,11 +1,33 @@
+/*
+ * Juego de Colores - Actividad principal que aloja los fragmentos
+ * Autor: Leonardo Pachari
+ * Fecha creación: 11/05/2025
+ * Última modificación: 11/05/2025
+ */
+
 package com.example.juegodecolores
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var navController: NavController
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        // Configuramos el controlador de navegación
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+    
+    // Manejo del botón de retroceso
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
